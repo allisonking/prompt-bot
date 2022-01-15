e512157f35986457ca7b09e426cf2e6c6879f001
Allison King <allisonjuliaking@gmail.com>
2022-01-15 17:20:46 -0500

    Add unsplash api

--- pb/bot.py
+++ pb/bot.py
@@ -4,6 +4,7 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from .constants import RANDOM_UNSPLASH
 from .util import read_prompts
 
 load_dotenv()
@@ -19,9 +20,10 @@ async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
 
 
-@bot.command(name="prompt", help="Responds with a prompt")
+@bot.command(name="prompt", help="Responds with a text prompt")
 async def prompt(ctx):
-    response = random.choice(BASE_PROMPT_LIST)
+    unsplash = RANDOM_UNSPLASH + f"?sig={random.randrange(1000)}"
+    response = random.choice(BASE_PROMPT_LIST + [unsplash])
     await ctx.send(response)
 
 
